6b35158b2a0ded2503e63235abaecc647e250e00
JackOatley <jack.f.oatley@gmail.com>
2019-03-06 18:29:24 +0000

    Demo room.

--- src/dgen.rs
+++ src/dgen.rs
@@ -3,10 +3,20 @@ pub mod generator {
 	use crate::tiles::*;
 
 	pub fn generate(map: &mut Map) {
-		for y in 1..map.height()-1 as usize {
-    	    for x in 1..map.width()-1 as usize {
-				map.set(x, y, Tile::empty());
-    		}
-    	}
+		rectangle(map, 1, 1, map.width()-1, map.height()-1, Tile::empty);
+
+		// Room test.
+		rectangle(map, 10, 10, 30, 20, Tile::wall);		// Wall.
+		rectangle(map, 20, 10, 21, 11, Tile::empty);	// Door.
+		rectangle(map, 11, 11, 29, 19, Tile::empty);	// Interior.
+		rectangle(map, 15, 15, 18, 18, Tile::gold);		// Goooooold.
+	}
+
+	fn rectangle(map: &mut Map, x1: usize, y1: usize, x2: usize, y2: usize, t: fn() -> Tile) {
+		for y in y1..y2 as usize {
+			for x in x1..x2 as usize {
+				map.set(x, y, t());
+			}
+		}
 	}
 }
